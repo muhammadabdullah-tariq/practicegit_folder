791a8ac3e14b501a2d80763d5e40f604469ae7ba
Your Name <youremail@example.com>
2026-02-13 02:25:53 +0500

    Initial commit

---
+++ 13/2/classdata.py
@@ -0,0 +1,12 @@
+class ClassName:
+    # The Constructor: Runs automatically when a new object is created
+    def __init__(self, parameter1, parameter2):
+        self.attribute1 = parameter1  # Assigns data to the specific object
+        self.attribute2 = parameter2
+
+    # A Method: A function that belongs to the class
+    def action(self):
+        return f"Processing {self.attribute1}..."
+    
+
+    
